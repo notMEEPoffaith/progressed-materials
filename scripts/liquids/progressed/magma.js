91221599cb5d2ccf2f27d344ab6e4b51cc90054a
meepnotoffiath <78182741+notMEEPoffaith@users.noreply.github.com>
2021-03-26 14:58:00 -0400

    made magma OP and BLOO

--- scripts/liquids/progressed/magma.js
+++ scripts/liquids/progressed/magma.js
@@ -4,12 +4,12 @@ const moltenRock = extendContent(Liquid, "magma", {
   }
 });
 moltenRock.localizedName = "Magma";
-moltenRock.description = "Actually, it's supposed to be lava...";
+moltenRock.description = "Actually, it's supposed to be lava... ...and notMEEPoffaith was here and made it H O T and have super low viscosity. Oh and it's also BLUE!";
 moltenRock.details = "How the heck are you reading this?";
-moltenRock.flammability = 1000;
-moltenRock.temperature = 1000;
+moltenRock.flammability = 1000000000;
+moltenRock.temperature = 1000000000;
 moltenRock.heatCapacity = 0;
-moltenRock.viscosity = 0.8;
-moltenRock.color = Color.valueOf("F58859");
-moltenRock.barColor = Color.valueOf("F58859");
-moltenRock.lightColor = Color.valueOf("F58859");
+moltenRock.viscosity = 0.1;
+moltenRock.color = Color.valueOf("59C6F5");
+moltenRock.barColor = Color.valueOf("59C6F5");
+moltenRock.lightColor = Color.valueOf("59C6F5");
